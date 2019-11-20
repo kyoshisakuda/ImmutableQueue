cb2a71c6777bf85b7b6da5c458cb48c2fd0fb785
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-11-19 22:01:20 -0500

    Add Element inner class

--- src/main/java/com/demo/util/ImmutableQueue.java
+++ src/main/java/com/demo/util/ImmutableQueue.java
@@ -2,6 +2,9 @@ package com.demo.util;
 
 public class ImmutableQueue<T> implements Queue<T> {
 
+    private Element<T> head;
+    private Element<T> tail;
+
     public Queue<T> enQueue(T t) {
         return this;
     }
@@ -16,6 +19,37 @@ public class ImmutableQueue<T> implements Queue<T> {
 
     public boolean isEmpty() {
         return false;
+    }
+
+    public Element<T> getHead() {
+        return head;
+    }
+
+    public Element<T> getTail() {
+        return tail;
+    }
+
+    class Element<T> {
+
+        private T value;
+        private Element<T> next;
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Element<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Element<T> next) {
+            this.next = next;
+        }
+
     }
 
 }
